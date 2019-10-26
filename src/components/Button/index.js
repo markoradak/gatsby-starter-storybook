e205b978de67afb60ecbd6ca69f0b5f8fc5ea213
Roberto Sousa <beto.sousa22@gmail.com>
2019-10-26 16:32:17 +0100

    add new button

---
+++ src/components/Button/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+
+const Button = ({ label, link }) => (
+  <Link to={link} style={{ 'padding-top': '20px' }}>
+    <ButtonWrapper type="button">{label}</ButtonWrapper>
+  </Link>
+);
+
+export default Button;
+
+const ButtonWrapper = styled.button`
+  min-width: 180px;
+  cursor: pointer;
+  text-align: center;
+  white-space: nowrap;
+  user-select: none;
+  font-size: 14px;
+  font-weight: 800;
+  line-height: 1;
+  color: rgb(255, 255, 255);
+  border-radius: 3em;
+  overflow: hidden;
+  padding: 13px 20px;
+  text-decoration: none;
+  background: #ff4785;
+`;
